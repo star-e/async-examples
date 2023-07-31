b872ef6ce24f2aa2b049790e647be86d3743da67
Zhou Zhenglong <zlzhou.sh@outlook.com>
2023-07-31 19:13:37 +0800

    add helloworld

---
+++ source/tutorial-000-helloworld/main.cpp
@@ -0,0 +1,23 @@
+#include <unifex/sync_wait.hpp>
+#include <unifex/just.hpp>
+#include <type_traits>
+#include <iostream>
+
+void test_combined() {
+    auto res = unifex::sync_wait(unifex::just(0));
+    static_assert(std::is_same_v<decltype(res), std::optional<int>>);
+    std::cout << "Result: " << *res << std::endl;
+}
+
+void test_separated() {
+    auto task = unifex::just(std::make_unique<int>(1));
+    auto res = unifex::sync_wait(std::move(task)); // Q: Can std::move be omitted?
+    // Q: What type is res?
+    std::cout << "Result: " << **res << std::endl;
+}
+
+int main() {
+    test_combined();
+    test_separated();
+    return 0;
+}
